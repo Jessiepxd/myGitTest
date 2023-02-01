e7f4f16631ba389a8233061871edb887b6f71ad0
Jie Chen <chenjiesunny@gmail.com>
2023-02-01 02:59:22 -0500

    1st commit

---
+++ src/mygittest/MyGitTest.java
@@ -0,0 +1,19 @@
+
+
+package mygittest;
+
+/**
+ * This class +++ Insert class description here +++
+ *
+ * @author Jie Chen
+ */
+public class MyGitTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int firstVersion;
+    }
+
+}
